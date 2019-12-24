91109fe0943c73e490a9c6e6a4ff534152dc8209
Mike Ayers <mgayers@yahoo.com>
2019-12-24 08:20:43 -0500

    After Video 7 (login/logout)

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 # Create your views here.
 
@@ -10,8 +11,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, "Account created for {}".format(username))
-            return redirect('blog-home')
+            messages.success(request, "Your Account has been created! {} can now login".format(username))
+            return redirect('blog-login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form':form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
